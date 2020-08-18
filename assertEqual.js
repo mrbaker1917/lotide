a003add3c39e17a52ebf4eede171a2d81b2dbde7
Mark Baker <mrbaker1917@gmail.com>
2020-08-18 01:28:37 +0000

    created script for testing assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  const f = String.fromCodePoint(0x1F92C);
+  const p = String.fromCodePoint(0x1F603);
+  if (actual === expected) {
+    console.log(`${p}${p}${p} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${f}${f}${f} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("dog", "dog");
+assertEqual("dog", "Dog");
+assertEqual(456, 456);
+assertEqual(456, 456.1);
