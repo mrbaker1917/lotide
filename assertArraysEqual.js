acdfb0b3302259eda3a100777d468995ebc4484a
Mark Baker <mrbaker1917@gmail.com>
2020-08-19 22:18:11 +0000

    added square bracket in output

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -11,9 +11,9 @@ const assertArraysEqual = (arr1, arr2) => {
   const f = String.fromCodePoint(0x1F92C);
   const p = String.fromCodePoint(0x1F603);
   if (eqArrays(arr1, arr2)) {
-    console.log(`${p}${p}${p} Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`${p}${p}${p} Assertion Passed: [${arr1}] === [${arr2}]`);
   } else {
-    console.log(`${f}${f}${f} Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`${f}${f}${f} Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
 };
 
