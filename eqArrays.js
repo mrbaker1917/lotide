4a275877de6aad64ba7f1ddf368f3d40db987a1b
Mark Baker <mrbaker1917@gmail.com>
2020-08-23 21:27:22 +0000

    improved eqArrays.js to handle infinite nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,24 @@
 const assertEqual = require('./assertEqual');
 
-const eqArrays = (arr1, arr2) => {
+const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if (Array.isArray(arr1[i])) {
+      if (eqArrays(arr1[i], arr2[i]) === false) {
+        return false;
+      }
+    } else {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
 };
 
 module.exports = eqArrays;
 
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should pass
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should pass
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should pass
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should pass
-// assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false); //=> should pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should pass
+assertEqual(eqArrays([1, 2, 3, [2, [4, [5], 6], 3, 4]], [1, 2, 3, [2, [4, [5], 6], 3, 4]]), true); //=> should pass
