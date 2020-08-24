957cdd1632955204c62dcb01e6f1886032a1043a
Mark Baker <mrbaker1917@gmail.com>
2020-08-24 20:03:25 +0000

    adds package.json, package-lock.json and headTest.js updated with mocha-chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(head([5,3,7,8]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 8);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 5 for [5, 3, 7, 8]", () => {
+    assert.strictEqual(head([5, 3, 7, 8]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("returns 7 for [7]", () => {
+    assert.strictEqual(head([7]), 7);
+  });
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
