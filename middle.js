d36c7da390c7b07e20d67380b532eaba839c8207
Mark Baker <mrbaker1917@gmail.com>
2020-08-24 15:01:15 +0000

    adds middleTest.js and updates middle.js with modules

--- middle.js
+++ middle.js
@@ -11,14 +11,4 @@ const middle = arr => {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']);
-assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e', 'f']), ['c', 'd']); // to make sure assertArrayEqual is working properly
-const arr3 = [4, 5, 6, 7, 8, 9];
-const result = middle(arr3);
-assertArraysEqual(result, [6, 7]); // this is to make sure middle() is not changing original array.
+module.exports = middle;
